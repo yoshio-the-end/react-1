39d2731dae26ab8e8eec6beef2432c7636d567ac
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-12-16 21:06:53 +0900

    Initial commit

---
+++ src/js/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+  render(){
+    return(
+      <footer>
+        <h1>footer</h1>
+      </footer>
+    );
+  }
+}
